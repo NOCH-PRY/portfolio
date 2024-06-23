d77f604653bc207aac5afaaa3994db52d593cca8
NO CH <115529042+NOCH-PRY@users.noreply.github.com>
2024-06-23 16:15:29 +0700

    Update Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className="header">
-      <h1>Your Name</h1>
+      <h1>NOCH-PRY{"</>"}</h1>
       <nav>
         <ul>
           <li>
